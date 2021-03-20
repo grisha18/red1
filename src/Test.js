2e070e464ee4a09adfa89bdc4c36df4219f7097b
grisha7777 <kurdjievgrigory777@gmail.com>
2021-03-20 21:08:54 +0300

    ref

--- src/Test.js
+++ src/Test.js
@@ -1,15 +1,19 @@
-import {useRef} from 'react';
+import React, {useRef} from 'react';
 
 const Test = ()=>{
 
-
+    const input = useRef(null);
     const Ref = useRef(null);
     return(
     <>
 
         
-        <input ref={Ref} type="text" placeholder="privet"></input>
+        <input ref={input} type="text" placeholder="privet"></input>
         <button onClick={()=>{Ref.current.focus()}}>poca</button>
+        <button onClick={()=>{console.log(input.current.value)
+         setTimeout( () =>{
+          button,  3000
+        })}}>press me</button>
 
         
     </>
